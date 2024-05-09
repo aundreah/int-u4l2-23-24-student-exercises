bdcf2743b13cd6e0c4742f1a95e3eb01d5c5e949
aundreah <149087999+aundreah@users.noreply.github.com>
2024-05-09 15:24:26 +0000

    5/9

--- concatenation.js
+++ concatenation.js
@@ -9,11 +9,13 @@ submitButton.addEventListener("click", function() {
     let birthYear = document.querySelector(".year").value;
     let age = 2023 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let city = document.querySelector(".city").value;
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
-
+results.innerHTML="My name is " +  myName;
+results.innerHTML=results.innerHTML + ". I'm feeling " + mood 
+results.innerHTML=results.innerHTML + " and I absolutely lovveeee eating " + food + " 😍..." 
+results.innerHTML=results.innerHTML + " I am " + age + " years old and I live in " + city + "."
 });
--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let captionOne = document.querySelector(".caption-one");
 
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
+    let spidyCaption;
     //  - Store the value of the input with a class of "comment-one".
-
+    buttonOne.addEventListener("click", function() {
+        let spidyCaption = document.querySelector(".comment-one").value; 
+        captionOne.textContent = spidyCaption;
+    });
 
     // 2. Display the variable spidyCaption to captionOne.
-
+        captionOne.textContent = spidyCaption;
 
 });
 
